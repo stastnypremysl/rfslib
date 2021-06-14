cd5bbe9fe02b77a938280736fe2d439ce1efa108
Přemysl Šťastný <p-w@stty.cz>
2021-06-14 15:04:53 +0200

    changing ssh libs to paramiko

--- src/rfslib/sftp_pconnection.py
+++ src/rfslib/sftp_pconnection.py
@@ -1,32 +1,53 @@
-import pysftp
+import paramiko
 from rfslib import abstract_pconnection
+
+from stat import S_ISDIR
 
 class SftpPConnection(abstract_pconnection.PConnection):
   def __init__(self, **arg):
     super().__init__(**arg)
 
-    if arg["password"] == None:
-      self.__sftp = pysftp.Connection(host=arg["host"], username=arg["username"], private_key=arg["keyfile"], port=arg["port"])
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    
+    host_key_policy = None
+    if arg["strict_host_key_checking"] == False:
+      host_key_policy = paramiko.client.WarningPolicy
+
     else:
-      self.__sftp = pysftp.Connection(host=arg["host"], username=arg["username"], password=arg["password"], port=arg["port"])
+      host_key_policy = paramiko.client.RejectPolicy
+
+    client.set_missing_host_key_policy = host_key_policy
+      
+    if arg["password"] == None:
+      client.connect(hostname=arg["host"], port=arg["port"], username=arg["username"], key_filename=arg["keyfile"])
+    else:
+      client.connect(hostname=arg["host"], port=arg["port"], username=arg["username"], password=arg["password"])
+
+    self.__sftp = client.open_sftp()
 
   def close(self):
     self.__sftp.close()
   
   def _listdir(self, remote_path):
-    return self.__sftp.listdir(remotepath=remote_path)
+    return self.__sftp.listdir(path=remote_path)
 
   def _rename(self, old_name, new_name):
     self.__sftp.rename(old_name, new_name) 
 
   def _push(self, local_path, remote_path):
-    self.__sftp.put(local_path, remotepath=remote_path)
+    self.__sftp.put(local_path, remote_path)
 
   def _pull(self, remote_path, local_path):
-    self.__sftp.get(remote_path, localpath=local_path)
+    self.__sftp.get(remote_path, local_path)
   
   def _isdir(self, remote_path):
-    return self.__sftp.isdir(remote_path)
+    result = False
+    try:
+      result = S_ISDIR(self._sftp.stat(remote_path).st_mode)
+    except IOError:     # no such file
+      result = False
+    return result
   
   def _mkdir(self, remote_path):
     self.__sftp.mkdir(remote_path, mode=744)
@@ -38,9 +59,18 @@ class SftpPConnection(abstract_pconnection.PConnection):
     self.__sftp.unlink(remote_path)
 
   def _exists(self, remote_path):
-    return self.__sftp.exists(remote_path)
+    try:
+      self.__sftp.stat(remotepath)
+    except IOError:
+      return False
+
+    return True
   
   def _lexists(self, remote_path):
-    return self.__sftp.lexists(remote_path)
+    try:
+      self._sftp.lstat(remotepath)
+    except IOError:
+      return False
+    return True
 
 
