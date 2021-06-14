6e92762f29b4974c8304c1cc439a22da437b92e9
Přemysl Šťastný <p-w@stty.cz>
2021-06-14 13:30:06 +0200

    added Setup.py, cleaned pglobber.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+  setuptools.setup(name='rfslib',
+    version='0.1.0',
+    description='Remote file system libraries for data manipulation between remote /and local host/ (cp, mv, ls, rm,..)',
+    long_description=long_description,
+    author='Přemysl Šťastný',
+    author_email='p-w@stty.cz',
+    url='https://git.profinit.eu/pstastny/rfslib',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+      'pysmb>=1,<2',
+      'paramiko>=2,<3',
+      'ConfigArgParse>=1,<2'
+    ]
+  )
