dd1348dfa296f3cb00c8d6e2fa10cabe26533b1b
Přemysl Šťastný <p-w@stty.cz>
2021-07-26 13:03:38 +0200

    FTP debug

--- src/rfslib/ftp_pconnection.py
+++ src/rfslib/ftp_pconnection.py
@@ -8,12 +8,12 @@ class FtpPConnection(abstract_pconnection.PConnection):
     super().__init__(**arg)
 
     if arg['tls']:
-      self.__ftp = ftplib.FTP_TLS(source_address=(arg["host"], arg["port"]), user=arg["username"], passwd=arg["password"])
+      self.__ftp = ftplib.FTP_TLS(source_address=(arg["host"], arg["port"]))
     else:
-      self.__ftp = ftplib.FTP(source_address=(arg["host"], arg["port"]), user=arg["username"], passwd=arg["password"])
+      self.__ftp = ftplib.FTP(source_address=(arg["host"], arg["port"]))
 
     self.__ftp.encoding='utf-8'
-    self.__ftp.sendcmd('OPTS UTF8 ON')
+    self.__ftp.login(user=arg["username"], passwd=arg["password"])
 
   def close(self):
     self.__ftp.quit()
