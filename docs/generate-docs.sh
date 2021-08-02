bd57fce52f39ef1f781572d2a77d0d8e6c0a572e
Přemysl Šťastný <p-w@stty.cz>
2021-08-02 15:43:42 +0200

    documentation typo

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -23,7 +23,7 @@ pip install $main_repo
   cat >$readme << EOF
 
 # rfstools documentation
-This is automatically generated documentation of [rfslib](https://git.profinit.eu/rfs/rfstools).
+This is automatically generated documentation of [rfslib](https://git.profinit.eu/rfs/rfslib).
 ## rfstools developer documentation
 The developer documentation is in file [rfslib-dev-doc.pdf](./rfslib-dev-doc.pdf).
 ## rfstools commands
